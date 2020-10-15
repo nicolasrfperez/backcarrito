acb6b727abcc9c2be49f149e188e6ad1cf9c9d4c
nico <nicolasrfperez@hotmail.com>
2020-10-14 21:06:42 -0300

    modificados cat cont

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,8 +3,12 @@ const categoriesModel = require("../models/categoriesModel");
 module.exports = {
     getAll: async(req, res, next) =>{
         console.log(req.query)
-        const producto = await categoriesModel.find();
-        res.json(producto);
+        try{
+            const producto = await categoriesModel.find();
+            res.json(producto);
+        }catch (e){
+            next(e)
+        }
     },
     getById: async (req, res, next) => {
         console.log(req.params.id);
@@ -17,11 +21,15 @@ module.exports = {
     },
     create: function(req, res, next){
         console.log(req.body);
-        const producto = new categoriesModel({
-            name: req.body.name,
-        })
-        producto.save();
-        res.json(producto);
+        try {    
+            const producto = new categoriesModel({
+                name: req.body.name,
+            })
+            producto.save();
+            res.json(producto);
+        }catch(e){
+            next (e)
+        }
     },
     delete: async function (req, res, next) {
         try{
